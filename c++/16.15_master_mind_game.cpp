68b004b3d8b98544b57d3721a680fbc559d582bb
QiuYuanhui <qiuyh0924@163.com>
2020-02-14 18:02:55 +0800

    feat(16.15): Game of Master Mind

---
+++ c++/16.15_master_mind_game.cpp
@@ -0,0 +1,28 @@
+/*
+ * Interview 16.15. The Game of Master Mind
+ */
+
+#include "header.h"
+
+class Solution {
+    public:
+        vector<int> masterMind(string solution, string guess) {
+            vector<int> res(2, 0);
+            map<char, int> dict_s;
+            map<char, int> dict_g;
+            for (int i = 0; i < 4; i++) {
+                if (solution[i] == guess[i]) {
+                    res[0]++;
+                } else {
+                    dict_g[guess[i]]++;
+                    dict_s[solution[i]]++;
+                }
+            }
+            for (auto it : dict_s) {
+                if (dict_g.count(it.first)) {
+                    res[1] += min(dict_g[it.first], dict_s[it.first]);
+                }
+            }
+            return res;
+        }
+};
