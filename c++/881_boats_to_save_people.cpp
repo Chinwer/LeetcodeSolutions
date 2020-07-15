acf52f9b972919c1fd8dfdac7226c339bbacf792
Chinwer <qiuyh0924@163.com>
2020-07-15 21:12:38 +0800

    feat(881): Boats to Save People

---
+++ c++/881_boats_to_save_people.cpp
@@ -0,0 +1,35 @@
+/*
+ * 881. Boats to Save People
+ *
+ * The i-th person has weight people[i], and each boat can carry a maximum weight of limit.
+ *
+ * Each boat carries at most 2 people at the same time, provided the sum of the weight of those people is at most limit.
+ *
+ * Return the minimum number of boats to carry every given person.  (It is guaranteed each person can be carried by a boat.)
+ *
+ * Note:
+ *
+ * 1 <= people.length <= 50000
+ * 1 <= people[i] <= limit <= 30000
+ */
+
+#include "header.h"
+
+class Solution {
+    public:
+        int numRescueBoats(vector<int>& people, int limit) {
+            sort(people.begin(), people.end());
+            int size = people.size();
+            int res = 0, left = 0, right = size - 1;
+            while (left <= right) {
+                res++;
+                if (people[left] + people[right] > limit) {
+                    right--;
+                } else {
+                    left++;
+                    right--;
+                }
+            }
+            return res;
+        }
+};
