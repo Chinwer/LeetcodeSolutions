31215358c38acf7fc416053e96167a97da4ff0ad
Chinwer <qiuyh0924@163.com>
2020-07-22 22:33:33 +0800

    feat(872): Leaf-Similar Trees

--- c++/872_leaf-similar_trees.cpp
+++ c++/872_leaf-similar_trees.cpp
@@ -22,6 +22,20 @@ struct TreeNode {
 class Solution {
     public:
         bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+            vector<int> leaves1, leaves2;
+            dfs(root1, leaves1);
+            dfs(root2, leaves2);
+            return leaves1 == leaves2;
+        }
 
+        void dfs(TreeNode *root, vector<int> &leaves) {
+            if (!root) {
+                return;
+            }
+            if (!root->left && !root->right) {
+                leaves.push_back(root->val);
+            }
+            dfs(root->left, leaves);
+            dfs(root->right, leaves);
         }
 };
