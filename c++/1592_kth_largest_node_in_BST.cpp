a15f80b1ee0a5d1c3dcf2f7d4ce77e6d9c2e5f05
Chinwer <qiuyh0924@163.com>
2020-07-22 20:05:16 +0800

    feat(1592): Kth Largest Node in BST

---
+++ c++/1592_kth_largest_node_in_BST.cpp
@@ -0,0 +1,36 @@
+/*
+ * 1592. Kth Largest Node in Binary Search Tree
+ */
+
+#include "header.h"
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+    private:
+        int res, cnt = 1;
+
+    public:
+        int kthLargest(TreeNode* root, int k) {
+            recursion(root, k);
+            return res;
+        }
+
+        void recursion(TreeNode *root, int k) {
+            if (!root) {
+                return;
+            }
+            recursion(root->right, k);
+            cnt++;
+            if (cnt == k) {
+                res = root->val;
+                return;
+            }
+            recursion(root->left, k);
+        }
+};
