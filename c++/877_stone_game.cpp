e4bf6ca2806950a98b0f77cacdc1591b29528ac5
Chinwer <qiuyh0924@163.com>
2020-07-25 16:22:51 +0800

    feat(877): Stone Game

---
+++ c++/877_stone_game.cpp
@@ -0,0 +1,45 @@
+/*
+ * 877. Stone Game
+ *
+ * Alex and Lee play a game with piles of stones.  There are an even number of piles arranged in a row, and each pile has a positive integer number of stones piles[i].
+ *
+ * The objective of the game is to end with the most stones.  The total number of stones is odd, so there are no ties.
+ *
+ * Alex and Lee take turns, with Alex starting first.  Each turn, a player takes the entire pile of stones from either the beginning or the end of the row.  This continues until there are no more piles left, at which point the person with the most stones wins.
+ *
+ * Assuming Alex and Lee play optimally, return True if and only if Alex wins the game.
+ *
+ * Note:
+ *   - 2 <= piles.length <= 500
+ *   - piles.length is even.
+ *   - 1 <= piles[i] <= 500
+ *   - sum(piles) is odd.
+ */
+
+#include "header.h"
+
+class Solution {
+    public:
+        bool stoneGame(vector<int>& piles) {
+            int n = piles.size();
+            vector<vector<pair<int, int>>> dp(n, vector(n, pair(0, 0)));
+            for (int i = 0; i < n; i++) {
+                dp[i][i].first = piles[i];
+                dp[i][i].second = 0;
+            }
+            for (int i = n - 2; i >= 0; i--) {
+                for (int j = i + 1; j < n; j++) {
+                    int left = piles[i] + dp[i + 1][j].second;
+                    int right = piles[j] + dp[i][j - 1].second;
+                    if (left > right) {
+                        dp[i][j].first = left;
+                        dp[i][j].second = dp[i + 1][j].first;
+                    } else {
+                        dp[i][j].first = right;
+                        dp[i][j].second = dp[i][j - 1].first;
+                    }
+                }
+            }
+            return dp[0][n - 1].first - dp[0][n - 1].second;
+        }
+};
