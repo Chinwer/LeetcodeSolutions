a2da42e8ed7496b536b11ab070837dfa1e8cb2e5
Chinwer <qiuyh0924@163.com>
2020-07-31 13:36:51 +0800

    feat(1227): Airplane Seat Assignment Probability

---
+++ c++/1227_airplane_seat_assignment.cpp
@@ -0,0 +1,22 @@
+/*
+ * 1227. Airplane Seat Assignment Probability
+ *
+ * n passengers board an airplane with exactly n seats. The first passenger has lost the ticket and
+ * picks a seat randomly. But after that, the rest of passengers will:
+ *   - Take their own seat if it is still available,
+ *   - Pick other seats randomly when they find their seat occupied
+ * What is the probability that the n-th person can get his own seat?
+ *
+ * Constraints:
+ *
+ * 1 <= n <= 10^5
+ */
+
+#include "header.h"
+
+class Solution {
+    public:
+        double nthPersonGetsNthSeat(int n) {
+            return n == 1 ? 1 : 0.5;
+        }
+};
