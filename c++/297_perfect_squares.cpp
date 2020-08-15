6546d10080f0ae70d31e9b6a039dfbe2a993e692
Chinwer <qiuyh0924@163.com>
2020-08-15 14:38:50 +0800

    feat(297): Perfect Squares

---
+++ c++/297_perfect_squares.cpp
@@ -0,0 +1,23 @@
+/*
+ * 297. Perfect Squares
+ *
+ * Given a positive integer n, find the least number of perfect square
+ * numbers (for example, 1, 4, 9, 16) which sum to n.
+ */
+
+#include "header.h"
+
+class Solution {
+    public:
+        int numSquares(int n) {
+            vector<int> dp(n + 1);
+            for (int i = 1; i <= n; i++) {
+                int min_ = INT_MAX;
+                for (int j = 1; j * j <= i; j++) {
+                    min_ = min(min_, dp[i - j * j] + 1);
+                }
+                dp[i] = min_;
+            }
+            return dp[n];
+        }
+};
